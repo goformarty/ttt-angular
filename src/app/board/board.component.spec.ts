3b88c507c37f65118d1d369ef5d23f98bc5aede8
Marty <marty.stepien@gmail.com>
2017-08-09 16:13:56 +0100

    board component created, methods for placing and returning a mark added and tested

---
+++ src/app/board/board.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardComponent } from './board.component';
+
+describe('BoardComponent', () => {
+  let component: BoardComponent;
+  let fixture: ComponentFixture<BoardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BoardComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('board construction', () => {
+    it('creates an empty board', () => {
+      expect(component.isBoardEmpty()).toEqual(true);
+    });
+  });
+
+  describe('making a move', () => {
+    it('places a mark on the board', () => {
+      const board = new BoardComponent();
+      board.placeMark('X', 1);
+      expect(board.returnMark(1)).toEqual('X');
+    });
+
+    it('does not place a mark outside the board', () => {
+      const board = new BoardComponent();
+      board.placeMark('X', -1);
+      expect(board.returnMark(-1)).toBeUndefined();
+    });
+  });
+});
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent {
+  private size = 9;
+  private grid: Array<any> = new Array<any>(this.size);
+
+  constructor() { }
+
+  isBoardEmpty(): boolean {
+    return (this.remainingMoves() === 0) ? true : false;
+  }
+
+  placeMark(mark: string, position: number) {
+    if (this.isValid(position)) {
+      this.grid[position] = mark;
+    }
+  }
+
+  returnMark(position: number) {
+    return this.grid[position];
+  }
+
+  private remainingMoves() {
+    let remaining = 0;
+    // tslint:disable-next-line:prefer-const
+    for (let i in this.grid) {
+      if ( this.grid[i] === undefined ) {
+       remaining++;
+      }
+    }
+    return remaining;
+  }
+
+  private isValid(position: number) {
+    return (position >= 0 && position <= 8);
+  }
+}
+
+
