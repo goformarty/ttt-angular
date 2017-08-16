da49443ae5840c74fcae3b9637e26be1b01a9bdb
Marty <marty.stepien@gmail.com>
2017-08-16 11:56:28 +0100

    toggle player method updated to refer to remainigMoves from board component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -14,13 +14,15 @@ export class GameComponent {
     this.board = board;
   }
 
+
   currentPlayer = 'X';
 
   makeMove(position: number) {
     if (this.board.isValid(position)) {
       this.board.placeMark(this.currentPlayer, position);
-      this.changePlayer();
+      this.toggleCurrentPlayer(this.board);
     }
+
   }
 
   isDraw() {
@@ -31,10 +33,6 @@ export class GameComponent {
     }
   }
 
-  checkingStuff(): any {
-    console.log(this.board.returnMark(1));
-  }
-
   isWon(): boolean {
     if (this.checkRows() || this.checkColumns() || this.checkDiagonals()) {
       return true;
@@ -42,35 +40,44 @@ export class GameComponent {
     return false;
   }
 
-  private changePlayer() {
-    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  private toggleCurrentPlayer(board: BoardComponent) {
+    // this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+
+    (this.board.remainingMoves() % 2 !== 0) ?
+    this.currentPlayer = 'X' :
+    this.currentPlayer = 'O';
+
   }
+
 
   private checkRows() {
-    for (let i = 1; i <= 7; i = i + 3) {
-      // tslint:disable-next-line:max-line-length
-      if (this.board.returnMark(i) !== undefined && this.board.returnMark(i) === this.board.returnMark(i + 1) && this.board.returnMark(i) === this.board.returnMark(i + 2)) {
-        return true;
-      }
+  for (let position = 1; position <= 7; position = position + 3) {
+    if (this.board.returnMark(position) !== undefined &&
+      this.board.returnMark(position) === this.board.returnMark(position + 1) &&
+      this.board.returnMark(position) === this.board.returnMark(position + 2)) {
+      return true;
     }
   }
+}
 
-    private checkColumns() {
-    for (let i = 1; i <= 3; i++) {
-      // tslint:disable-next-line:max-line-length
-      if (this.board.returnMark(i) !== undefined && this.board.returnMark(i) === this.board.returnMark(i + 3) && this.board.returnMark(i + 3) === this.board.returnMark(i + 6)) {
-        return true;
-      }
+  private checkColumns() {
+  for (let i = 1; i <= 3; i++) {
+    if (this.board.returnMark(i) !== undefined &&
+      this.board.returnMark(i) === this.board.returnMark(i + 3) &&
+      this.board.returnMark(i + 3) === this.board.returnMark(i + 6)) {
+      return true;
     }
   }
+}
 
   private checkDiagonals() {
-    for (let i = 1, j = 4; i <= 3; i = i + 2, j = j - 2) {
-      // tslint:disable-next-line:max-line-length
-      if (this.board.returnMark(i) !== undefined && this.board.returnMark(i) === this.board.returnMark(i + j) && this.board.returnMark(i + j) === this.board.returnMark(i + 2 * j)) {
-        return true;
-      }
+  for (let i = 1, j = 4; i <= 3; i = i + 2, j = j - 2) {
+    if (this.board.returnMark(i) !== undefined &&
+      this.board.returnMark(i) === this.board.returnMark(i + j) &&
+      this.board.returnMark(i + j) === this.board.returnMark(i + 2 * j)) {
+      return true;
     }
   }
+}
 
 }
